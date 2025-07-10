3e77fa99a04df740909878775ff81a6d3675f9ba
govindmaddala2107 <maddala.govind2107@gmail.com>
2025-07-10 20:32:49 +0530

    ms setup

---
+++ employees_ms/src/main/java/com/gomad/employees_ms/EmployeesMsApplication.java
@@ -0,0 +1,13 @@
+package com.gomad.employees_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesMsApplication.class, args);
+	}
+
+}
---
+++ employees_ms/src/test/java/com/gomad/employees_ms/EmployeesMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomad.employees_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
